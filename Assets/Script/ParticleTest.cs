97c033d54115f296cd165c4ea568c76ea999ce12
Pietro Biasotti <biasotp@gmail.com>
2025-01-24 21:46:10 +0100

    scripts

---
+++ Assets/Script/ParticleTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ParticleTest : MonoBehaviour
+{
+    public ParticleSystem part;
+
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "Player")
+            Debug.Log(other.tag);
+    }
+}
